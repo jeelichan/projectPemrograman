157149df09aa5f71920b33547e3ffc084f420316
jeelichan <114078447+jeelichan@users.noreply.github.com>
2024-10-08 14:30:26 +0000

    Change the data types

--- PerencanaanDanaPensiun.cpp
+++ PerencanaanDanaPensiun.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 #include <cmath>
-#define ld long double
 #define ll long long
-
 using namespace std;
 
 // Fungsi untuk menghitung setoran bulanan
-ld hitungSetoranBulanan(ld targetPensiun, double sukuBungaTahunan, int tahunHinggaPensiun, int frekuensiSetoran) {
+ll hitungSetoranBulanan(ll targetPensiun, double sukuBungaTahunan, int tahunHinggaPensiun, int frekuensiSetoran) {
     double sukuBungaBulanan = sukuBungaTahunan / frekuensiSetoran;
     ll jumlahSetoran = tahunHinggaPensiun * frekuensiSetoran;
-    ld setoranBulanan = targetPensiun * sukuBungaBulanan / (pow(1 + sukuBungaBulanan, jumlahSetoran) - 1);
+    ll setoranBulanan = targetPensiun * sukuBungaBulanan / (pow(1 + sukuBungaBulanan, jumlahSetoran) - 1);
     return setoranBulanan;
 }
 
 // Fungsi untuk mensimulasikan total pensiun
-ld simulasiTotalPensiun(ld setoranBulanan, double sukuBungaTahunan, int tahunHinggaPensiun, int frekuensiSetoran) {
+ll simulasiTotalPensiun(ll setoranBulanan, double sukuBungaTahunan, int tahunHinggaPensiun, int frekuensiSetoran) {
     double sukuBungaBulanan = sukuBungaTahunan / frekuensiSetoran;
     ll jumlahSetoran = tahunHinggaPensiun * frekuensiSetoran;
-    ld totalPensiun = 0;
+    ll totalPensiun = 0;
     for (int i = 0; i < jumlahSetoran; i++) {
         totalPensiun = (totalPensiun + setoranBulanan) * (1 + sukuBungaBulanan);
         if ((i + 1) % frekuensiSetoran == 0) {
@@ -28,7 +26,8 @@ ld simulasiTotalPensiun(ld setoranBulanan, double sukuBungaTahunan, int tahunHin
 }
 
 int main() {
-    ld targetPensiun, sukuBungaTahunan;
+    ll targetPensiun;
+    double sukuBungaTahunan;
     int tahunHinggaPensiun, frekuensiSetoran=12;
 
     cout << "Program Perencanaan Pensiun" << endl;
@@ -40,12 +39,11 @@ int main() {
     cout << "Masukkan tahun hingga pensiun: ";
     cin >> tahunHinggaPensiun;
 
-    ld setoranBulanan = hitungSetoranBulanan(targetPensiun, sukuBungaTahunan, tahunHinggaPensiun, frekuensiSetoran);
-    ld totalPensiun = simulasiTotalPensiun(setoranBulanan, sukuBungaTahunan, tahunHinggaPensiun, frekuensiSetoran);
+    ll setoranBulanan = hitungSetoranBulanan(targetPensiun, sukuBungaTahunan, tahunHinggaPensiun, frekuensiSetoran);
+    ll totalPensiun = simulasiTotalPensiun(setoranBulanan, sukuBungaTahunan, tahunHinggaPensiun, frekuensiSetoran);
 
     cout << "Setoran bulanan: " << setoranBulanan << endl;
     cout << "Total pensiun: " << totalPensiun << endl;
 
     return 0;
 }
-
